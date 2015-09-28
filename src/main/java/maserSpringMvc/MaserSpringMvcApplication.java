1e0269ccda8c4cb4b4a875ba7599fa1be3be7ede
Jiri Michal <jirimichal82@gmail.com>
2015-09-28 14:14:53 +0200

    Initial commit

---
+++ src/main/java/maserSpringMvc/MaserSpringMvcApplication.java
@@ -0,0 +1,12 @@
+package maserSpringMvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaserSpringMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaserSpringMvcApplication.class, args);
+    }
+}
